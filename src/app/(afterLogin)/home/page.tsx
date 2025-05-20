26893a9e681db1a02729b87148f980e4f70c6073
HoonJae <rlagnswo0505@naver.com>
2025-05-20 22:02:30 +0900

    feat: home 작업 중

--- src/app/(afterLogin)/home/page.tsx
+++ src/app/(afterLogin)/home/page.tsx
@@ -1,9 +1,18 @@
+import { Input } from '@/components/ui/input';
+import { Search } from 'lucide-react';
 import React from 'react';
 
 const page = () => {
   return (
     <div>
-      <section></section>
+      <section>
+        <form className="fixed w-[inherit]">
+          <div className="bg-muted-foreground/10 flex items-center justify-start rounded-full">
+            <Search className="text-muted-foreground absolute top-2 left-2 transform" />
+            <Input className="rounded-full pl-10" name="q" />
+          </div>
+        </form>
+      </section>
       <section></section>
       <section></section>
       <section></section>
